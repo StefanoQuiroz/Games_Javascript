ef0fce52d9d25aa8aaecd91238a12cf9e0253e91
Piero <piero.quiroz93@gmail.com>
2021-04-02 18:32:12 -0500

    Ninja Walking

--- pre_Games/ninja_I/script.js
+++ pre_Games/ninja_I/script.js
@@ -1,23 +1,34 @@
+var topValue= 150, leftValue=150, direction="down", step=1;
+
 function update(){
-    document.getElementById("ninja").style.left = leftValue+"px";
-    document.getElementById("ninja").style.top = topValue+"px"; 
+    if(step === 1){
+        step = 2;
+    } else {
+        step = 1;
+    }
+    document.getElementById("ninja").style.left= leftValue+"px"; 
+    document.getElementById("ninja").style.top = topValue+"px";
+    document.getElementById("ninja").style.backgroundImage = "url(./image/"+direction+step+".png)"  
 }
-var leftValue = 750, topValue= 150;
+
 document.onkeydown = function(e){
-    console.log(e);
     console.log(e.keyCode);
-    // 37 left
-    // 39 right
-    // 40 down
-    // 38 up
+    //left 37
+    //right 39
+    //up 38
+    //down 40
     if(e.keyCode === 37 && leftValue > 0){
-        leftValue -=10;
-    } else if(e.keyCode === 39 && leftValue < 350){
-        leftValue +=10;
-    } else if(e.keyCode === 40 && topValue < 350){
-        topValue +=10;
+        leftValue -= 10;
+        direction = "left";
+    } else if(e.keyCode === 39 && leftValue < 900){
+        leftValue += 10;
+        direction = "right";
     } else if(e.keyCode === 38 && topValue > 0){
-        topValue -=10;
+        topValue -= 10;
+        direction = "top"
+    } else if(e.keyCode === 40 && topValue < 750){
+        topValue += 10;
+        direction = "down"
     }
     update();
 }
